ba6141cd8d27151c658e60c1bc7313e267096752
harrisonyei <ydharrison0720@gmail.com>
2017-04-08 03:51:19 -0500

    main file

---
+++ veryBigCal/BigCalculator/BigCalculator/BigObject/library/NumberObject.h
@@ -0,0 +1,18 @@
+// NumberObject 是所有大數型別的基本類別
+
+class NumberObject{
+public:
+	NumberObject();
+	~NumberObject();
+
+private:
+
+};
+
+NumberObject::NumberObject()
+{
+}
+
+NumberObject::~NumberObject()
+{
+}
