c897838aecc68b3d928356957e3f94e0b9275c84
unknown <>
2017-04-10 21:14:48 +0800

    separate complex into real part and imaginary part

--- veryBigCal/BigCalculator/BigCalculator/BigObject/Complex.cpp
+++ veryBigCal/BigCalculator/BigCalculator/BigObject/Complex.cpp
@@ -1,7 +1,53 @@
 
+// Name: ¶À¯EºÕ 
+// Student ID: B10515007
+// Date: April 10, 2017 
+// Last Update: Date: April 10, 2017
+// Problem statement: This C++ program separate complex into real part and imaginary part.
 
-#include"library\Complex.h"
+#include <string>
+
+#include "../Operation/TestCore.h"
+#include "library\Complex.h"
 
 void Complex::StoC(std::string s) {
 
+	//clear white character
+	TestCore::ClearWhite(s);
+
+	int operatorPos = 0;
+	char sign = 0;
+	std::string realprt;
+	std::string imaginaryprt;
+
+	//find position of operator
+	for (int i = 1; i < s.size(); i++) {
+		if (s[i] == '+' || s[i] == '-') {
+			sign = s[i];
+			operatorPos = i;
+			break;
+		}
+	}
+	//get substring from begin to position of operator
+	realprt = s.substr(0, operatorPos);
+
+	//get substring from next position of operator to the end
+	imaginaryprt = s.substr(operatorPos + 1);
+
+	//delete 'i' from imaginary part
+	imaginaryprt.erase(imaginaryprt.begin() + imaginaryprt.size() - 1);
+
+	//a - -bi = a + bi, a - bi = a + -bi
+	if (sign == '-') {
+
+		if (imaginaryprt[0] == '-') {
+			imaginaryprt.erase(imaginaryprt.begin() + 0);
+		}
+		else {
+			imaginaryprt.insert(0, "-");
+		}
+	}
+
+	strReal = realprt;
+	strImag = imaginaryprt;
 }
